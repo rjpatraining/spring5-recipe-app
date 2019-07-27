577c28e2ea482da96bdea50326651d5cf35dda5f
Rajmohan P <rajmohan.p@tavant.com>
2019-07-27 15:12:30 -0500

    done

---
+++ src/main/java/com/rjpard/spring5recipeapp/controller/IndexController.java
@@ -0,0 +1,14 @@
+package com.rjpard.spring5recipeapp.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+
+
+    @RequestMapping({"","/","/index"})
+    public String getIndexPage(){
+        return "index";
+    }
+}
